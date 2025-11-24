4c1256d85ecc873409698cd0a9c3903e3d39800f
pxxja <poojabhambid2004@gmail.com>
2025-11-24 15:41:02 +0530

    define route to verify otp

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import RegisterAdmin from "./pages/Register";
 import Login from "./pages/Login";
+import VerifyOTP from "./pages/VerifyOtp";
 
 const App: React.FC = () => {
   return (
     <>
       <Routes>
         <Route path="/register" element={<RegisterAdmin />} />
+        <Route path="/verify-otp" element={<VerifyOTP />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </>
