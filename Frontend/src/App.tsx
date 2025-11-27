ce93c20dcd5e4d33257059e0695804ab5f5aab7c
pxxja <poojabhambid2004@gmail.com>
2025-11-27 19:52:14 +0530

    define routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import RegisterAdmin from "./pages/Register";
 import Login from "./pages/Login";
-import VerifyOTP from "./pages/VerifyOtp";
-
+import VerifyOTP from "./pages/VerifyOTP";
+import Home from "./pages/Home"
 const App: React.FC = () => {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home/>}/>
         <Route path="/register" element={<RegisterAdmin />} />
         <Route path="/verify-otp" element={<VerifyOTP />} />
         <Route path="/login" element={<Login />} />
