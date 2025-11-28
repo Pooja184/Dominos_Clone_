e741f3ebc41678ccab16777c84de96df5c8691ee
pxxja <poojabhambid2004@gmail.com>
2025-11-28 12:02:28 +0530

    routes for authentication

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import { getCurrentUser, loginUser, logoutUser, registerUser, verifyOtp } from "../controllers/auth.controller.js";
-import { authMiddleware } from "../middlewares/tokenDecode.js";
+import {  login, register, verifyOtp } from "../controllers/auth.controller.js";
+import { tokenDecoder } from "../middlewares/tokenDecode.js";
 // import authRouter from "./auth.route";
 
 const authRouter = Router();
 
-authRouter.post("/register", registerUser);
+authRouter.post("/register", register);
 authRouter.post("/verify-otp", verifyOtp);
-authRouter.post("/login", loginUser);
-authRouter.get("/me",authMiddleware, getCurrentUser)
-authRouter.post("/logout", logoutUser);
+authRouter.post("/login", login);
+// authRouter.get("/me",authMiddleware, getCurrentUser)
+// authRouter.post("/logout", logoutUser);
 
 export default authRouter;
