1987c98110c6dae653f0efaa9fa3a56783c024c6
pxxja <poojabhambid2004@gmail.com>
2025-11-28 18:33:10 +0530

    entry point for products

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import authRouter from "../routes/auth.ts";
+import authRouter from "../routes/auth.js";
+import productRouter from "./product.js";
 
 const mainRouter = Router();
 
 mainRouter.use("/auth", authRouter);
+mainRouter.use("/product", productRouter);
+
 
 export default mainRouter;
