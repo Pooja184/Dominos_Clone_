23b24de8311a7cbafc0c540de132500bbf74e7f2
pxxja <poojabhambid2004@gmail.com>
2025-11-28 18:32:19 +0530

    schema for products

---
+++ backend/models/seller/product.model.ts
@@ -0,0 +1,45 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IProduct extends Document {
+  name: string;
+  description: string;
+  category: string;
+  type: "veg" | "non-veg";
+
+  price: number;
+  originalPrice?: number;
+  discount?: number;
+
+  image: string;
+  details?: string[];
+
+  sellerId: mongoose.Types.ObjectId;   // RELATION
+}
+
+const productSchema = new Schema<IProduct>(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+
+    category: { type: String, required: true },
+    type: { type: String, enum: ["veg", "non-veg"], required: true },
+
+    price: { type: Number, required: true },
+    originalPrice: Number,
+    discount: Number,
+
+    image: { type: String, required: true },
+
+    details: [{ type: String }],
+
+    // ⭐ RELATION WITH SELLER COLLECTION
+    sellerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Seller",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IProduct>("Product", productSchema);
