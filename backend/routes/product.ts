8c87fc9d8aedd1db24a1f42473cf2e02ad99bb7d
pxxja <poojabhambid2004@gmail.com>
2025-11-29 01:30:37 +0530

    route for get-products

--- backend/routes/product.ts
+++ backend/routes/product.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 import { tokenDecoder } from "../middlewares/tokenDecode.js";
 import { isRoleSeller } from "../middlewares/isRoleSeller.js";
 import { upload } from "../config/multer.js";
-import { addProduct } from "../controllers/seller/seller.controller.js";
+import { addProduct, getSellerProducts } from "../controllers/seller/seller.controller.js";
 // import authRouter from "./auth.route";
 
 const productRouter = Router();
 
 productRouter.post('/add-product',tokenDecoder,isRoleSeller,upload.single("image"),addProduct)
-
+productRouter.get("/get-products", tokenDecoder, isRoleSeller, getSellerProducts)
 
 export default productRouter;
