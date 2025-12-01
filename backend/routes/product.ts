f519511472dd77d3ea294496bae46b26ced10f10
pxxja <poojabhambid2004@gmail.com>
2025-12-01 15:06:54 +0530

    route for delete product

--- backend/routes/product.ts
+++ backend/routes/product.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 import { tokenDecoder } from "../middlewares/tokenDecode.js";
 import { isRoleSeller } from "../middlewares/isRoleSeller.js";
 import { upload } from "../config/multer.js";
-import { addProduct, getSellerProducts } from "../controllers/seller/seller.controller.js";
+import { addProduct, deleteProduct, getSellerProducts } from "../controllers/seller/seller.controller.js";
 // import authRouter from "./auth.route";
 
 const productRouter = Router();
 
 productRouter.post('/add-product',tokenDecoder,isRoleSeller,upload.single("image"),addProduct)
 productRouter.get("/my-products", tokenDecoder, isRoleSeller, getSellerProducts)
-
+productRouter.delete("/delete/:id",tokenDecoder,deleteProduct)
 export default productRouter;
