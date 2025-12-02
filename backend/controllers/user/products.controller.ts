f4a7fc11eb4882ffaeb1d0e97c30bfa79590fe11
pxxja <poojabhambid2004@gmail.com>
2025-12-02 19:22:00 +0530

    create get category products api

---
+++ backend/controllers/user/products.controller.ts
@@ -0,0 +1,25 @@
+import type { Request, Response } from "express";
+import Product from "../../models/seller/product.model.js";
+
+
+export const getCategoryProducts = async (req: Request, res: Response) => {
+  try {
+    const { category } = req.params;
+    
+    
+    if (!category) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Category is required" });
+    }
+
+    const products = await Product.find({ category });
+
+    res.json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
