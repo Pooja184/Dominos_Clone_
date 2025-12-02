b698f141360155c236e07f25f2f2ef3b60f22199
pxxja <poojabhambid2004@gmail.com>
2025-12-02 19:20:50 +0530

    built add category and get category feature

--- backend/controllers/seller/product.controller.ts
+++ backend/controllers/seller/product.controller.ts
@@ -1,5 +1,8 @@
 import type{ Request, Response } from "express";
 import Product from "../../models/seller/product.model.js";
+import { uploadToCloudinary } from "../../config/cloudinary.js";
+import fs from "fs";
+import { Category } from "../../models/categories.model.js";
 
 export const GetProducts = async (req:Request, res:Response) => {
   try {
@@ -12,3 +15,53 @@ export const GetProducts = async (req:Request, res:Response) => {
       .send({ message: "Error in getting current user", success: false });
   }
 };
+
+
+export const addCategories=async(req:Request,res:Response)=>{
+  try {
+    const {name}=req.body.toLowerCase();
+
+    if(!name){
+      return res.status(400).json({
+        success:false,
+        message:"please enter name"
+      })
+    }
+
+    if(!req.file){
+      return res.status(400).json({
+        success:false,
+        message:"image is required"
+      })
+    }
+
+    const cloudinaryImageUrl=await uploadToCloudinary(req.file.path);
+
+    fs.unlinkSync(req.file.path);
+
+    await Category.create({
+      name,
+      image:cloudinaryImageUrl.url,
+      imagePublicId:cloudinaryImageUrl.public_id
+    })
+      return res.status(201).json({
+      success: true,
+      message: "Category added successfully",
+    });
+  } catch (error) {
+    console.log("Add category Error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+}
+
+export const getCategories=async(req:Request,res:Response)=>{
+  try{
+    const categories=await Category.find({});
+    return res.status(200).json({success:true,categories})
+  }catch(error){
+    res.status(500).json({message:"Error in getting categories",success:false})
+  }
+}
