02edc3204adc23e108d7fc3fdf74f213c4de051e
pxxja <poojabhambid2004@gmail.com>
2025-12-03 23:31:33 +0530

    add categories component in home page

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import Navbar from '../components/Navbar.tsx'
+import Categories from './Categories.tsx'
 const Home = () => {
   return (
     <>
       <Navbar/>
+      <Categories/>
     </>
   )
 }
