16b87683652cd34b48f00de6a446f5fe6f836330
pxxja <poojabhambid2004@gmail.com>
2025-12-04 23:31:17 +0530

    add get products

--- backend/controllers/user/products.controller.ts
+++ backend/controllers/user/products.controller.ts
@@ -23,3 +23,15 @@ export const getCategoryProducts = async (req: Request, res: Response) => {
     res.status(500).json({ success: false, message: "Server error" });
   }
 };
+
+export const GetAllProducts = async (req:Request, res:Response) => {
+  try {
+    const products = await Product.find({});
+    return res.json({ success: true, products });
+  } catch (error) {
+    console.log("error", error);
+    res
+      .status(500)
+      .send({ message: "Error in getting current user", success: false });
+  }
+};
